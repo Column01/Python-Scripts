669a7d96d3c844a226dd6223daadca8f9e4d0130
Column01 <ColinAndress999@gmail.com>
2020-04-15 00:47:57 -0400

    COC Collector changes
    - Changed image samples to full tags to help reduce false positives.
    - Added dark elixir as its own scanner since it sometimes did not get triggered by the elixir scanner
    - Tweaked detection values

--- COC Collector/README.md
+++ COC Collector/README.md
@@ -17,9 +17,9 @@ This does not prevent your from getting kicked when your villagers need a break!
 5. Zoom ALL the way out on your base by pressing the down arrow key or by pressing `Ctrl + -` until it is all the way zoomed out.
 5. Download this script and the dependencies listed above.
 6. Run the script using `python collector.py`
-7. It should detect gold, and elixir bubbles (both types) and automatically click them. 
+7. It should detect gold, and elixir bubbles (both types) and automatically click them. Dark elixir and normal elixir may trigger each other since they are so similar. I can't find a way around it yet, but it's not a huge deal.
 
 ### Notes
 - If it doesn't detect one of the resource types, make sure you have the display settings set properly and you are zoomed all the way out.
-- If it detects false positives or still doesn't detect a resource type, you may need to tweak the detection percentage in the script. It can be found in the `find_gold` and `find_elixirs` functions labeled `detection_value`. It must not exceed `1.0` and I don't reccommend anything lower than `0.50`
-- The detection might not work on some monitor resolutions. It was designed on a 1080p resolution so any monitor with a lower or higher resolution may not detect the bubble properly. If that is the case, you can copy the image samples I included by zooming all the way out on your base and screenshotting the gold and elixir collection bubbles. Then you just need to cut out all but the resource icon like I have done here and keep the file names the same and it should work fine.
+- If it detects false positives or still doesn't detect a resource type, you may need to tweak the detection percentage in the script. It can be found in the `find_gold`, `find_dark_elixir` and `find_elixir` functions labeled `detection_value`. It must not exceed `1.0` and I don't reccommend anything lower than `0.50`
+- The detection might not work on some monitor resolutions. It was designed on a 1080p resolution so any monitor with a lower or higher resolution may not detect the bubble properly. If that is the case, you can copy the image samples I included by zooming all the way out on your base and screenshotting the gold, dark elixir and elixir collection bubbles. Then you just need to rename the files to the same as I have it and it should work fine.
--- COC Collector/collector.py
+++ COC Collector/collector.py
@@ -6,15 +6,15 @@ import pyautogui
 
 global running
 running = False
-threads = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 elixir_path = os.path.join(__location__, "elixir.png")
 gold_path = os.path.join(__location__, "gold.png")
+dark_elixir_path = os.path.join(__location__, "elixir.png")
 
 
 def find_gold():
     global running
-    detection_value = 0.70
+    detection_value = 0.65
     while running:
         gold_scan = None
         while gold_scan is None:
@@ -30,21 +30,39 @@ def find_gold():
         time.sleep(3)
 
 
-def find_elixirs():
+def find_elixir():
     global running
-    detection_value = 0.60
+    detection_value = 0.65
     while running:
         elixir_scan = None
         while elixir_scan is None:
             if running is not False:
                 elixir_scan = pyautogui.locateOnScreen(elixir_path, confidence=detection_value, grayscale=True)
                 if elixir_scan is not None:
-                    print("Found a type of elixir bubble on screen. Clicking it.")
+                    print("Found an elixir bubble on screen. Clicking it.")
                     click_point = pyautogui.center(elixir_scan)
                     pyautogui.click(click_point.x, click_point.y)
             else:
                 return
-        print("Waiting 3 seconds then we will start scanning for the elixir bubbles again.")    
+        print("Waiting 3 seconds then we will start scanning for the elixir again.")    
+        time.sleep(3)
+        
+
+def find_dark_elixir():
+    global running
+    detection_value = 0.60
+    while running:
+        dark_elixir_scan = None
+        while dark_elixir_scan is None:
+            if running is not False:
+                dark_elixir_scan = pyautogui.locateOnScreen(dark_elixir_path, confidence=detection_value, grayscale=True)
+                if dark_elixir_scan is not None:
+                    print("Found a dark elixir bubble on screen. Clicking it.")
+                    click_point = pyautogui.center(dark_elixir_scan)
+                    pyautogui.click(click_point.x, click_point.y)
+            else:
+                return
+        print("Waiting 3 seconds then we will start scanning for dark elixir again.")    
         time.sleep(3)
             
             
@@ -53,9 +71,10 @@ def main():
     running = True
     gold_thread = threading.Thread(target=find_gold)
     gold_thread.start()
-    elixir_thread = threading.Thread(target=find_elixirs)
+    elixir_thread = threading.Thread(target=find_elixir)
     elixir_thread.start()
-    threads.extend([gold_thread, elixir_thread])
+    dark_elixir_thread = threading.Thread(target=find_dark_elixir)
+    dark_elixir_thread.start()
     try:
         while True:
             pass
