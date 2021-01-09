2d1a6cd30e9086f024edac0be9a6ace112f6bd50
Column01 <colinandress999@gmail.com>
2021-01-08 22:42:17 -0500

    Update instagram downloader and README
    It is no longer functional do to changes in the instagram frontend.

--- Instagram Downloader/README.md
+++ Instagram Downloader/README.md
@@ -1,4 +1,7 @@
-# Instagram Downloader
+# Instagram Downloader (NOT FUNCTIONAL)
+
+**NOTE:** Changes to the instagram frontend has made this tool non functional. I do not intend to fix it, this is here as an example for python code.
+
 Download a picture or video from instagram
 
 Run using: `python instagram_downloader.py`
--- Instagram Downloader/instagram_downloader.py
+++ Instagram Downloader/instagram_downloader.py
@@ -1,7 +1,7 @@
 import requests
 import shutil
 import re
-import json
+
 
 print("-" * 25)
 print("\nThanks for using this script. Note: Videos will not be downloaded and must be downloaded via the link this script provides (should be easy enough to figure out :wink:). Images will be downloaded as \"output.jpg\"\n")
@@ -9,9 +9,7 @@ print("-" * 25)
 user_type = input("Image or Video? (I or V): ")
 user_type = user_type.lower()
 user_input = input("Provide an instagram post link please: ")
-headers = {
-        "cookie": "none"
-    }
+headers = {"cookie": "none"}
 if headers["cookie"] == 'none':
     print("Cookie is not set. Private pictures and videos will not work!")
     print("Please obtain cookie by looking in the Chrome developer tools and then under the Application tab or alternatively obtain your cookie from your instagram network traffic when loading the instagram webpage")
@@ -38,18 +36,8 @@ if user_type == "v":
     req = requests.get(user_input, headers=headers)
     for line in req.text.split("\n"):
         l = line.strip()
-        test = re.match('<script type="text/javascript">window.__initialDataLoaded', l)
+        test = re.search('\"video_url\"[:](\".*\")', l)
         if test is not None:
-            l = l.split(",", 1)[1] \
-                 .replace("true", "1") \
-                 .replace("false", "0") \
-                 .strip(");</script><script type=\"text/javascript\"")
-            try:
-                test2 = json.loads(l)
-                print("Video URL: " + test2["graphql"]["shortcode_media"]["video_url"])
-                exit(0)
-            except json.JSONDecodeError as e:
-                print("Cannot parse JSON from payload. Instagram may have changed their site to stop this link gathering method. Error: ")
-                print(e)
-                exit(1)
+            test2 = test.group().split(",", 1)[0].replace("video_url", "").replace('"', "").replace(":", "", 1)
+            print("Video URL: {}".format(test2))
     print("Account may be private")
